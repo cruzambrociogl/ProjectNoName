231b368d3f9e734df0f4f174790fee8c64c808dc
CruzAmbrocio <cruz.ambrocio1409@gmail.com>
2022-11-17 17:12:19 -0600

    React native initialization

---
+++ src/navigation/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+// views import
+//import Loading from '../views/Loading'
+
+
+const Views = createSwitchNavigator({
+
+});
+
+export default createAppContainer(Views);
